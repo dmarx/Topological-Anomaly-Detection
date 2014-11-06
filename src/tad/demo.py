64a9e19d9cc68f5a2353647eb9b1d35dc5b45604
davidmarx <david.marx@datamininglab.com>
2014-11-06 13:53:50 -0500

    Indented plotting

--- src/tad/demo.py
+++ src/tad/demo.py
@@ -22,19 +22,19 @@ if True:
     scatter_matrix(df.ix[:,:4], c=df.anomaly, s=(25 + 50*df.anomaly), alpha=.8)
     plt.show()
 
-print 'Anomalies:', res['classed']['anomalies']
-g = res['g']
-X_pca = PCA().fit_transform(df)
-pos = dict((i,(X_pca[i,0], X_pca[i,1])) for i in range(X_pca.shape[0]))
-colors = [node[1]['color'] for node in g.nodes(data=True)]
-labels = {}
-print len(g.nodes())
-for node in g.nodes():
-    if node in res['classed']['anomalies']:
-        labels[node] = node
-    else:
-        labels[node] = ''
-nx.draw(g, pos=pos, node_color = colors, labels=labels)
-nx.draw_networkx_labels(g, pos=pos, node_color = colors, labels=labels)
-plt.show()
+    print 'Anomalies:', res['classed']['anomalies']
+    g = res['g']
+    X_pca = PCA().fit_transform(df)
+    pos = dict((i,(X_pca[i,0], X_pca[i,1])) for i in range(X_pca.shape[0]))
+    colors = [node[1]['color'] for node in g.nodes(data=True)]
+    labels = {}
+    print len(g.nodes())
+    for node in g.nodes():
+        if node in res['classed']['anomalies']:
+            labels[node] = node
+        else:
+            labels[node] = ''
+    nx.draw(g, pos=pos, node_color = colors, labels=labels)
+    nx.draw_networkx_labels(g, pos=pos, node_color = colors, labels=labels)
+    plt.show()
 
