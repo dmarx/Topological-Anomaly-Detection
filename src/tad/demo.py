8db6f875284495a6b78f113cb6a63adeac42559f
davidmarx <david.marx@datamininglab.com>
2014-11-06 14:01:25 -0500

    Updated demo.py to match master branch

--- src/tad/demo.py
+++ src/tad/demo.py
@@ -5,16 +5,10 @@ from pandas.tools.plotting import scatter_matrix
 from sklearn import datasets
 from sklearn.decomposition import PCA
 from TADClassifier import tad_classify
-import time
 
 iris = datasets.load_iris()
 df = pd.DataFrame(iris.data)
-
-start = time.time()
 res = tad_classify(df.values)
-print "Elapsed: {t}".format(t=time.time()-start)
-
-print res['scores']
 
 plot = False
 if plot:
@@ -23,19 +17,19 @@ if plot:
     scatter_matrix(df.ix[:,:4], c=df.anomaly, s=(25 + 50*df.anomaly), alpha=.8)
     plt.show()
 
-    print 'Anomalies:', res['classed']['anomalies']
+    print 'Anomalies:', res['outliers']
     g = res['g']
     X_pca = PCA().fit_transform(df)
     pos = dict((i,(X_pca[i,0], X_pca[i,1])) for i in range(X_pca.shape[0]))
-    colors = [node[1]['color'] for node in g.nodes(data=True)]
+    colors = []
     labels = {}
-    print len(g.nodes())
     for node in g.nodes():
-        if node in res['classed']['anomalies']:
+        if node in res['outliers']:
             labels[node] = node
+            colors.append('r')
         else:
             labels[node] = ''
-    nx.draw(g, pos=pos, node_color = colors, labels=labels)
-    nx.draw_networkx_labels(g, pos=pos, node_color = colors, labels=labels)
+            colors.append('b')
+    nx.draw(g, pos=pos, node_color = colors)#, labels=labels)
+    nx.draw_networkx_labels(g,pos,labels)
     plt.show()
-
