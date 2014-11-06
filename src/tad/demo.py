3c59d36e5cf83de994099f9b57e570a1c2d2a9c8
davidmarx <david.marx@datamininglab.com>
2014-11-06 13:43:50 -0500

    Vectorized anomaly scoring, removed inliers from return value, updated demo for new data model and current networkx

--- src/tad/demo.py
+++ src/tad/demo.py
@@ -8,23 +8,27 @@ from TADClassifier import tad_classify
 
 iris = datasets.load_iris()
 df = pd.DataFrame(iris.data)
-res = tad_classify(df)
+res = tad_classify(df.values)
 
-df['anomaly']=0
-df.anomaly.ix[res['classed']['anomalies']] = 1
-scatter_matrix(df.ix[:,:4], c=df.anomaly, s=(25 + 50*df.anomaly), alpha=.8)
-plt.show()
+if True:
+    df['anomaly']=0
+    df.anomaly.ix[res['outliers']] = 1
+    scatter_matrix(df.ix[:,:4], c=df.anomaly, s=(25 + 50*df.anomaly), alpha=.8)
+    plt.show()
 
-print 'Anomalies:', res['classed']['anomalies']
-g = res['g']
-X_pca = PCA().fit_transform(df)
-pos = dict((i,(X_pca[i,0], X_pca[i,1])) for i in range(X_pca.shape[0]))
-colors = [node[1]['color'] for node in g.nodes(data=True)]
-labels = {}
-for node in g.nodes():
-    if node in res['classed']['anomalies']:
-        labels[node] = node
-    else:
-        labels[node] = ''
-nx.draw(g, pos=pos, node_color = colors, labels=labels)
-plt.show()
+    print 'Anomalies:', res['outliers']
+    g = res['g']
+    X_pca = PCA().fit_transform(df)
+    pos = dict((i,(X_pca[i,0], X_pca[i,1])) for i in range(X_pca.shape[0]))
+    colors = []
+    labels = {}
+    for node in g.nodes():
+        if node in res['outliers']:
+            labels[node] = node
+            colors.append('r')
+        else:
+            labels[node] = ''
+            colors.append('b')
+    nx.draw(g, pos=pos, node_color = colors)#, labels=labels)
+    nx.draw_networkx_labels(g,pos,labels)
+    plt.show()
